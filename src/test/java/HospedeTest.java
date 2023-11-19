b528a82b6fe462ec7a13768f80cea5c9cc3e760b
chelGomes <68570496+chelGomes@users.noreply.github.com>
2023-11-19 00:40:19 -0300

    implementacao casos de teste

--- src/test/java/HospedeTest.java
+++ src/test/java/HospedeTest.java
@@ -1,2 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class HospedeTest {
+    @Test
+    void deveElogiarSecretaria() {
+        Aluno aluno = new Aluno();
+        assertEquals("A Ouvidoria agradece seu contato.\nA Secretaria respondeu sua demanda conforme mensagem a seguir.\n" +
+                        ">>A Secretaria agradece a mensagem: Ótimo atendimento",
+                aluno.elogiarSecretaria("Ótimo atendimento"));
+    }
+
+    @Test
+    void deveReclamarSecretaria() {
+        Aluno aluno = new Aluno();
+        assertEquals("A Ouvidoria agradece seu contato.\nA Secretaria respondeu sua demanda conforme mensagem a seguir.\n" +
+                        ">>A Secretaria vai procurar melhorar o serviço da reclamação: Lentidão no atendimento",
+                aluno.reclamarSecretaria("Lentidão no atendimento"));
+    }
+
+    @Test
+    void deveSugerirSecretaria() {
+        Aluno aluno = new Aluno();
+        assertEquals("A Ouvidoria agradece seu contato.\nA Secretaria respondeu sua demanda conforme mensagem a seguir.\n" +
+                        ">>A Secretaria vai analisar a sugestão: Ampliar horário funcionamento",
+                aluno.sugerirSecretaria("Ampliar horário funcionamento"));
+    }
 }
