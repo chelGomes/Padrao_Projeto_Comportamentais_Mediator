700dd8197b303e87dc263c468ddea0953aceddc4
chelGomes <68570496+chelGomes@users.noreply.github.com>
2023-11-23 19:36:37 -0300

    modelagem

--- src/test/java/HospedeTest.java
+++ src/test/java/HospedeTest.java
@@ -4,26 +4,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HospedeTest {
     @Test
-    void deveElogiarSecretaria() {
-        Aluno aluno = new Aluno();
-        assertEquals("A Ouvidoria agradece seu contato.\nA Secretaria respondeu sua demanda conforme mensagem a seguir.\n" +
-                        ">>A Secretaria agradece a mensagem: Ótimo atendimento",
-                aluno.elogiarSecretaria("Ótimo atendimento"));
+    void deveElogiarHospedagem() {
+        Hospede hospede = new Hospede();
+        assertEquals("O Sistema de Reservas agradece seu contato.\nA equipe de hospedagem respondeu sua demanda conforme mensagem a seguir.\n" +
+                        ">>A equipe de hospedagem agradece a mensagem: Ótimo atendimento",
+                hospede.realizarReserva("Ótimo atendimento",hospede));
     }
 
     @Test
-    void deveReclamarSecretaria() {
-        Aluno aluno = new Aluno();
-        assertEquals("A Ouvidoria agradece seu contato.\nA Secretaria respondeu sua demanda conforme mensagem a seguir.\n" +
-                        ">>A Secretaria vai procurar melhorar o serviço da reclamação: Lentidão no atendimento",
-                aluno.reclamarSecretaria("Lentidão no atendimento"));
+    void deveReclamarHospedagem() {
+        Hospede hospede = new Hospede();
+        assertEquals("O Sistema de Reservas agradece seu contato.\nA equipe de hospedagem respondeu sua demanda conforme mensagem a seguir.\n" +
+                        ">>A equipe de hospedagem vai procurar melhorar o serviço da reclamação: Lentidão no atendimento",
+                hospede.cancelarReserva("Lentidão no atendimento",hospede));
     }
 
     @Test
-    void deveSugerirSecretaria() {
-        Aluno aluno = new Aluno();
-        assertEquals("A Ouvidoria agradece seu contato.\nA Secretaria respondeu sua demanda conforme mensagem a seguir.\n" +
-                        ">>A Secretaria vai analisar a sugestão: Ampliar horário funcionamento",
-                aluno.sugerirSecretaria("Ampliar horário funcionamento"));
+    void deveSugerirHospedagem() {
+        Hospede hospede = new Hospede();
+        assertEquals("O Sistema de Reservas agradece seu contato.\nA equipe de hospedagem respondeu sua demanda conforme mensagem a seguir.\n" +
+                        ">>A equipe de hospedagem vai analisar a sugestão: Ampliar horário de check-in/check-out",
+                hospede.realizarReserva("Ampliar horário de check-in/check-out",hospede));
     }
 }
